aa2d1e8d15f7c8b6a283ee5871500bf929f8a3dd
NickSchmahl <61160778+NickSchmahl@users.noreply.github.com>
2020-02-17 22:44:05 +0100

    Update View.java

--- View.java
+++ View.java
@@ -1,4 +1,35 @@
+import java.awt.Color;
 
-public class View {
+import acm.graphics.GOval;
+import acm.graphics.GRect;
+import acm.program.*;
+
+public class View extends GraphicsProgram {
+
+	private GRect background;
+	private GOval stone;
+	private GOval stones[][] = new GOval[6][7];
+
+	public View() {
+		background = new GRect(getWidth(), getHeight());
+		background.setFilled(true);
+		background.setColor(Color.WHITE);
+		add(background);
+
+		double size = getHeight() / 8 - 5;
+
+		for (int i = 5; i >= 0; i--) {
+			for (int j = 0; j < 7; j++) {
+				double x = (j + 1) * getWidth() / 9;
+				double y = ((5 - i) + 1) * getHeight() / 8;
+				stone = new GOval(x, y, size, size);
+				stone.setFilled(true);
+				stone.setColor(Color.BLACK); // get the information from the model
+												// about the color
+				stones[5 - i][j] = stone;
+				add(stones[5 - i][j]);
+			}
+		}
+	}
 
 }
